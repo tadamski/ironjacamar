cc204c0654df31ee891099f0d7e92c1d039cd8e5
Tomasz Adamski <tadamski@redhat.com>
2018-05-23 15:59:39 +0200

    Update header license information

--- adapters/src/main/java/org/jboss/jca/adapters/jdbc/extensions/oracle/OracleStaleConnectionChecker.java
+++ adapters/src/main/java/org/jboss/jca/adapters/jdbc/extensions/oracle/OracleStaleConnectionChecker.java
@@ -5,14 +5,13 @@
  * distribution for a full listing of individual contributors.
  *
  * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
+ * under the terms of the Eclipse Public License 1.0 as
+ * published by the Free Software Foundation.
  *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Eclipse
+ * Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this software; if not, write to the Free
